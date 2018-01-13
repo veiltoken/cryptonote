06b25f6b97d0e0b285eec29fa42f10165011eca0
Tony Seing <_@tonyseing.com>
2018-01-13 09:50:11 +0700

    changes name to veil

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -72,8 +72,7 @@ const char     CRYPTONOTE_BLOCKCHAIN_INDICES_FILENAME[]      = "blockchainindice
 const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
 } // parameters
 
-//TODO Put here the name of your currency
-const char     CRYPTONOTE_NAME[]                             = "";
+const char     CRYPTONOTE_NAME[]                             = "veil";
 const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
